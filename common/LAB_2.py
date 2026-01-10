acef7980eef88decec6ed40c09cb5777037aefb0
PhilbyChen <2084118196@qq.com>
2026-01-11 00:14:40 +0800

    26.1.11

--- common/LAB_2.py
+++ common/LAB_2.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+from astropy.wcs import WCS
 
 ## 2 Data I/O
 def load_fits(filepath, extension = 0, *args):
@@ -28,25 +29,80 @@ def load_fits(filepath, extension = 0, *args):
         hdu = hdul[extension]
         return hdu.header.copy(), hdu.data
 
-def implot(image, figsize=(15, 13), cmap ='gray_r', scale = 0.5, **kwargs):
+def strip_SIP(header):
+    A_prefixes = [i for i in header.keys() if i.startswith('A_')]
+    B_prefixes = [i for i in header.keys() if i.startswith('B_')]
+    for a,b in zip(A_prefixes,B_prefixes):
+        del header[a]
+        del header[b]
+    return header
+
+def implot(image, figsize=(15, 13), cmap ='gray_r', scale = 0.5, 
+           colorbar = False, header = None, wcs = None, 
+           **kwargs):
     '''
-    input:
-    image (2D array or masked array)
-    cmap 颜色映射名称--反向灰度
-    scale 对比度缩放因子
-    output:
-    使用 ax.imshow() 来实际绘制图像
-    用 im = ax.imshow(...)保存输出
+    Plot an astronomical image, setting default options and easy tweaking of parameters
+    
+    Parameters
+    ----------
+    image: array_like
+        2D array containing an astronomical image to be plotted. Cutouts should be input as cutout.data.
+    figsize: tuple, optional
+        figure size to use. Default: (15,13)
+    cmap: str, optional
+        Colormap to use for the image. Default: 'gray_r'
+    scale: float, optional
+        By default, function will scale image to some number of standard deviations about the mean pixel value. Scale sets this number (or fraction). Default: 0.5.
+    colorbar: bool, optional
+        Whether to add a colorbar or not. Default: False
+    header: dict, optional
+        If input, function will attempt to create a WCS object from header and plot in celestial coordinates. Default: None
+    wcs: WCS object
+        If input, the function will plot using a projection set by the WCS. Default: None
+    **kwargs
+        Additional arguments are passed to matplotlib plotting commands. Currently supported: vmin, vmax.
+        
+    Returns
+    -------
+    fig, ax
+        figure and axes objects containing currently plotted data.
     '''
-    fig, ax = plt.subplots(figsize = figsize)
-    mu = np.nanmedian(image)      # 平均值
-    sigma = np.nanstd(image)    # 标准差
+    header = strip_SIP(header)
+    # 如果提供了header但没提供wcs，从header创建wcs
+    # if header is not None 和 if header 不一样！
+    final_wcs = None
+    
+    if wcs is not None:
+        final_wcs = wcs
+    elif header is not None:
+        try:
+            # 先复制header，再修复，避免修改原始数据
+            header_copy = header.copy()
+            header_copy = strip_SIP(header_copy)
+            final_wcs = WCS(header_copy)
+        except:
+            final_wcs = None
+    # 创建图形和坐标轴，如果提供了wcs则使用投影.
+    if final_wcs is not None:  # 检查wcs
+        fig, ax = plt.subplots(figsize=figsize, subplot_kw={'projection': final_wcs})
+    else:
+        fig, ax = plt.subplots(figsize=figsize)
+
     # 计算基于scale的默认值
     default_vmin = np.nanpercentile(image, 5)
     default_vmax = np.nanpercentile(image, 98)
     vmin = kwargs.pop('vmin', default_vmin)
     vmax = kwargs.pop('vmax', default_vmax)
     im = ax.imshow(image, cmap = cmap, origin='lower', vmin=vmin, vmax=vmax)
+
+    if final_wcs is not None:
+        ax.coords[0].set_axislabel('pos.eq.ra')
+        ax.coords[1].set_axislabel('pos.eq.dec')
+
+    if colorbar:
+        plt.colorbar(im, ax = ax)
+
+    ax.grid(True, color='black', alpha=0.3, linestyle='--', linewidth=0.5)
 
     return fig, ax, im
 
@@ -60,23 +116,7 @@ print(f"数据类型: {data.dtype}")
 print(f"像素值范围: [{np.min(data):.2f}, {np.max(data):.2f}]")
 print(f"数据形状: {data.shape}")
 
-fig, ax, im = implot(image=data, figsize=(10, 10), cmap='gray_r', scale=0.5)
+fig, ax, im = implot(image=data, figsize=(10, 10), cmap='gray_r', scale=0.5, header = header)
 
 plt.tight_layout()
 plt.show()
-
-
-'''
-## 2.3
-
-在本节中，我们将允许（可选地）在图形中添加颜色条。
-我们还将添加功能，使图形能够在天球坐标（即 RA 和 DEC）而不是像素单位下绘制，如果图像信息（通过世界坐标系统，WCS）存在于图像头信息中。
-
-给你的函数添加三个新的可选参数。
-colorbar = False
-header = None
-wcs = None
-
-让我们从色条开始。在你的绘图命令结束后，检查 colorbar=True ，如果存在，则通过 plt.colorbar() 创建色条，将 mappable 参数设置为之前保存 ax.imshow() 输出的内容。
-同时将 ax 参数设置为你的 ax；这将告诉 matplotlib 从该轴中占用一些空间来放置色条。
-'''
