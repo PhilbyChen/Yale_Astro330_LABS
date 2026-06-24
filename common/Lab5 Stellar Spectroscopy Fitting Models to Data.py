421a995a4eeda2ac8c7cc2890bab65c39022c200
PhilbyChen <2084118196@qq.com>
2026-06-24 22:10:41 +0800

    2026.6.24
    the beginning of LAB5

---
+++ common/Lab5 Stellar Spectroscopy Fitting Models to Data.py
@@ -0,0 +1,26 @@
+"""Lab 5: Stellar Spectroscopy + Fitting Models to Data"""
+
+"""在本实验中，我们将指导您读取、绘制和拟合银河系球状星团中恒星的光谱。
+科学目标是确定少数恒星的速度及其误差，从而判断它们是球状星团的成员，还是银河系的前景恒星。
+编程目标是在模型较为复杂时，应用 χ2 拟合和 MCMC 拟合技术。"""
+
+"""Goals of this lab:  本实验的目标：¶
+Explore a maintained software package (pypeit).
+探索一个维护良好的软件包 pypeit。
+Read a complicated fits file and plot a spectrum.
+读取复杂的 fits 文件并绘制光谱图。
+Find parameters and errors via chi2 fitting when the model is not an analytic function
+当模型不是解析函数时，通过卡方拟合找到参数和误差
+Find parameters and errors via MCMC.
+利用 MCMC 方法查找参数和误差。
+Fitting polynomials to 2D surfaces, corner plots
+将多项式拟合到二维曲面，角图"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+from scipy.ndimage import gaussian_filter1d
+
+from astropy.io import fits
+file = 'spec1d_DE.20110603.45055-n7006a_DEIMOS_2011Jun03T123053.021.fits'
